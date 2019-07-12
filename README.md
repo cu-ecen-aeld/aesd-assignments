c5b608b9ea0df424009615a485af42e17f376948
Dan Walkes <danwalkes@trellis-logic.com>
2019-07-12 08:37:46 -0600

    Assignment 1 base code

---
+++ README.md
@@ -0,0 +1,3 @@
+# aesd-assignments
+This repo contains public starter source code, scripts, and documentation for AESD assignments
+See the github classroom assignment start instructions for details about how to use this repository
---
+++ tester.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Tester script for assignment 1.
+# Author: Siddhant Jajoo
+
+set -e
+set -u
+
+NUMFILES=10
+WRITESTR=ECEN_5013_IS_AWESOME
+WRITEDIR=/tmp/ecen5013
+MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
+OUTPUTFILE=assignments/assignment1/assignment1-result.txt
+
+
+if [ $# -lt 2 ]
+then
+	echo "Using default value ${WRITESTR} for string to write"
+	if [ $# -lt 1 ]
+	then
+		echo "Using default value ${NUMFILES} for number of files to write"
+	else
+		NUMFILES=$1
+	fi	
+else
+	NUMFILES=$1
+	WRITESTR=$2
+fi
+
+echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
+
+
+rm -rf "${WRITEDIR}"
+mkdir -p "$WRITEDIR"
+
+#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
+#The quotes signify that the entire string in WRITEDIR is a single string.
+#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
+if [ -d "$WRITEDIR" ]
+then
+	echo "$WRITEDIR created"
+else
+	exit 1
+fi
+
+# YOUR CODE HERE: the example implementation writes only one file
+# You need to modify it to write the number of files passed as the NUMFILES parameter
+./writer.sh "$WRITEDIR/$WRITESTR$i" "$WRITESTR"
+
+mkdir -p `dirname ${OUTPUTFILE}`
+
+./finder.sh "$WRITEDIR" "$WRITESTR" > "${OUTPUTFILE}"
+
+set +e
+grep "${MATCHSTR}" ${OUTPUTFILE}
+if [ $? -eq 0 ]; then
+	echo "success"
+	exit 0
+else
+	echo "failed: expected  ${MATCHSTR} in ${OUTPUTFILE} but instead found"
+	cat ${OUTPUTFILE}
+	exit 1
+fi

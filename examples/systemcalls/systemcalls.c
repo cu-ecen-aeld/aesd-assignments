a36ed5141e53b45707588855a3e811eac00cacb3
Akshita Bhasin <akshita.bhasin@colorado.edu>
2020-06-16 23:13:37 -0600

    Updates for assignment 2

---
+++ examples/systemcalls/systemcalls.c
@@ -0,0 +1,97 @@
+#include "systemcalls.h"
+
+/**
+* @param count - The numbers of variables passed to the function. The variables are command to execute.  Since system() performs path expansion, this does not need to be a full path to the command.
+* @param ...- A list of 0 or more arguments after the @param count function.  These will be appended to a command string before calling system() with a space separating each parameter.
+* @return true if the command @param ... with arguments @param arguments were executed successfully using the system() call, false if an error occurred, either in invocation of the system() command, or if a non-zero return value was returned by the command issued in @param.
+*/
+bool do_system(int count, ...)
+{
+    va_list args;
+    va_start(args, count);
+    const char * command;
+    int i;
+    bool null_flag =  false;
+    for(i=0; i<count; i++)
+    {
+        command = va_arg(args, const char *);
+        if(command == NULL)
+        {
+            null_flag = true;
+        }
+
+    }
+
+//TODO:
+/*
+*   Call the system() function with the commands passed in the @param command,
+*   and returns a boolean true if the system() call completed with success or false() if it returned a failure
+*
+*/
+
+    va_end(args);
+    
+    return true;
+}
+
+/**
+* @param count -The numbers of variables passed to the function. The variables are command to execute.  Since exec() does not perform path expansion, this needs to be a full path to the command.
+* @param ... - A list of 0 or more arguments after the @param count function.  These will be added to argv[1] and later in the const char * argument to execv (argv[0] will always be the command).
+* @return true if the command @param ... with arguments @param arguments were executed successfully using the execv() call, false if an error occurred, either in invocation of the fork, waitpid, or execv() command, or if a non-zero return value was returned by the command issued in @param arguments with the specified arguments
+*/
+
+bool do_exec(int count, ...)
+{
+    va_list args;
+    va_start(args, count);
+    char * command[count+1];
+    int i;
+    for(i=0; i<count; i++)
+    {
+        command[i] = va_arg(args, char *);
+    }
+    command[count] = NULL;
+
+// TODO:
+/*
+*   Calls fork, execv(), and wait instead of system (see LSP page 161).
+*   It should use the command value passed
+*   as the full path to the command to execute (first argument of execv). 
+*   
+*/
+
+    va_end(args);
+
+    return true;
+}
+
+/**
+* @param outputfile - The full path to the file to write with command output.  This file will be closed at completion of the function call.
+* @param count -The numbers of variables passed to the function. The variables are command to execute.  Since exec() does not perform path expansion, this needs to be a full path to the command.
+* @param ... - A list of 0 or more arguments after the @param count function.  These will be added to argv[1] and later in the const char * argument to execv (argv[0] will always be the command).
+* @return true if the command @param ... with arguments @param arguments were executed successfully using the system() call, false if an error occurred, either in invocation of the fork, waitpid, or execv() command, or if a non-zero return value was returned by the command issued in @param arguments.
+*/
+bool do_exec_redirect(const char *outputfile, int count, ...)
+{
+    va_list args;
+    va_start(args, count);
+    char * command[count+1];
+    int i;
+    for(i=0; i<count; i++)
+    {
+        command[i] = va_arg(args, char *);
+    }
+    command[count] = NULL;
+
+// TODO:
+/*
+*   Open the outputfile, fork a child process.
+*   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
+*   redirect standard out to the outputfile, and rest of the behaviour is same as do_exec()
+*   
+*/
+
+    va_end(args);
+    
+    return true;
+}
--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -6,10 +6,10 @@
 
 /**
 * This function should:
-*   1) Call the my_username() function in autotest-validate.c to get your hard coded username.
+*   1) Call the my_username() function in Test_assignment_validate.c to get your hard coded username.
 *   2) Obtain the value returned from function malloc_username_from_conf_file() in username-from-conf-file.h within
 *       the assignment autotest submodule at assignment-autotest/test/assignment1/
-*   3) Use unity assertion TEST_ASSERT_EQUAL_STRING_MESSAGE to verify the two strings are equal.  See
+*   3) Use unity assertion TEST_ASSERT_EQUAL_STRING_MESSAGE the two strings are equal.  See
 *       the [unity assertion reference](https://github.com/ThrowTheSwitch/Unity/blob/master/docs/UnityAssertionsReference.md)
 */
 void test_validate_my_username()

b41e5b149f96de26bb80a8ad2959b3b11722a291
Dan Walkes <danwalkes@trellis-logic.com>
2020-06-27 22:33:57 -0600

    Fix linewrap on systemcalls file

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,9 +1,17 @@
 #include "systemcalls.h"
 
 /**
-* @param count - The numbers of variables passed to the function. The variables are command to execute.  Since system() performs path expansion, this does not need to be a full path to the command.
-* @param ...- A list of 0 or more arguments after the @param count function.  These will be appended to a command string before calling system() with a space separating each parameter.
-* @return true if the command @param ... with arguments @param arguments were executed successfully using the system() call, false if an error occurred, either in invocation of the system() command, or if a non-zero return value was returned by the command issued in @param.
+* @param count - The numbers of variables passed to the function.
+*   The variables include command to execute with system() and the
+*   arguments to use for the command
+* @param ...- A list of 1 or more arguments after the @param count function.  
+*    Since system() performs path expansion, this does not need to be a full path to the command.
+*    These will be appended to a command string before calling system() 
+*    with a space separating each parameter.
+* @return true if the commands in ... with arguments @param arguments were executed 
+*   successfully using the system() call, false if an error occurred, 
+*   either in invocation of the system() command, or if a non-zero return 
+*   value was returned by the command issued in @param.
 */
 bool do_system(int count, ...)
 {
@@ -22,11 +30,11 @@ bool do_system(int count, ...)
 
     }
 
-//TODO:
 /*
-*   Call the system() function with the commands passed in the @param command,
-*   and returns a boolean true if the system() call completed with success or false() if it returned a failure
-*
+ * TODO  add your code here
+ *  Call the system() function with the commands set in the command variable
+ *   and return a boolean true if the system() call completed with success 
+ *   or false() if it returned a failure
 */
 
     va_end(args);
@@ -35,9 +43,15 @@ bool do_system(int count, ...)
 }
 
 /**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.  Since exec() does not perform path expansion, this needs to be a full path to the command.
-* @param ... - A list of 0 or more arguments after the @param count function.  These will be added to argv[1] and later in the const char * argument to execv (argv[0] will always be the command).
-* @return true if the command @param ... with arguments @param arguments were executed successfully using the execv() call, false if an error occurred, either in invocation of the fork, waitpid, or execv() command, or if a non-zero return value was returned by the command issued in @param arguments with the specified arguments
+* @param count -The numbers of variables passed to the function. The variables are command to execute.  
+*   Since exec() does not perform path expansion, the commands need to full paths.
+* @param ... - A list of 1 or more arguments after the @param count argument.
+*   These will be added to argv[1] and later in the const char * argument 
+*   to execv (argv[0] will always be the command).
+* @return true if the command @param ... with arguments @param arguments were executed successfully
+*   using the execv() call, false if an error occurred, either in invocation of the 
+*   fork, waitpid, or execv() command, or if a non-zero return value was returned
+*   by the command issued in @param arguments with the specified arguments.
 */
 
 bool do_exec(int count, ...)
@@ -52,12 +66,14 @@ bool do_exec(int count, ...)
     }
     command[count] = NULL;
 
-// TODO:
 /*
-*   Calls fork, execv(), and wait instead of system (see LSP page 161).
-*   It should use the command value passed
-*   as the full path to the command to execute (first argument of execv). 
-*   
+ * TODO:
+ *   Execute a system command by calling fork, execv(),
+ *   and wait instead of system (see LSP page 161).
+ *   Use the command[0] as the full path to the command to execute
+ *   (first argument to execv), and use the remaining arguments
+ *   as arguments to the systeem() command.
+ *   
 */
 
     va_end(args);
@@ -66,10 +82,20 @@ bool do_exec(int count, ...)
 }
 
 /**
-* @param outputfile - The full path to the file to write with command output.  This file will be closed at completion of the function call.
-* @param count -The numbers of variables passed to the function. The variables are command to execute.  Since exec() does not perform path expansion, this needs to be a full path to the command.
-* @param ... - A list of 0 or more arguments after the @param count function.  These will be added to argv[1] and later in the const char * argument to execv (argv[0] will always be the command).
-* @return true if the command @param ... with arguments @param arguments were executed successfully using the system() call, false if an error occurred, either in invocation of the fork, waitpid, or execv() command, or if a non-zero return value was returned by the command issued in @param arguments.
+* @param outputfile - The full path to the file to write with command output.  
+*   This file will be closed at completion of the function call.
+* @param count -The numbers of variables passed to the function. 
+*   The variables are command to execute and arguments to the command. 
+*   Since exec() does not perform path expansion, the first argument needs to be
+*   the full path to the command
+* @param ... - A list of 0 or more arguments after the @param count function.
+*   These will be added to argv[1] and later in the const char * argument
+*   to execv (argv[0] will always be the command).
+* @return true if the command @param ... with arguments @param arguments 
+*   were executed successfully using the system() call, false if an error occurred,
+*   either in invocation of the fork, waitpid, or execv() command,
+*   or if a non-zero return value was returned by the command issued in 
+*   arguments.
 */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
@@ -83,12 +109,12 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     }
     command[count] = NULL;
 
-// TODO:
 /*
-*   Open the outputfile, fork a child process.
-*   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
-*   redirect standard out to the outputfile, and rest of the behaviour is same as do_exec()
-*   
+ * TODO
+ *   Open the outputfile, fork a child process.
+ *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
+ *   redirect standard out to the outputfile, and rest of the behaviour is same as do_exec()
+ *   
 */
 
     va_end(args);

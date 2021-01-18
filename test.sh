244e8c66f2a165c910ca0474488d8dc276d2910a
Steve Rizor <steve.rizor@colorado.edu>
2021-01-18 00:53:31 -0700

    Expanded to allow testing multiple assignments

--- test.sh
+++ test.sh
@@ -29,21 +29,23 @@ set +e
 # additional tests
 if [ -f conf/assignment.txt ]; then
     # This is just one example of how you could find an associated assignment
-    assignment=`cat conf/assignment.txt`
-    if [ -f ./assignment-autotest/test/${assignment}/assignment-test.sh ]; then
-        echo "Executing assignment test script"
-        ./assignment-autotest/test/${assignment}/assignment-test.sh $test_dir
-        rc=$?
-        if [ $rc -eq 0 ]; then
-            echo "Test of assignment ${assignment} complete with success"
-        else
-            echo "Test of assignment ${assignment} failed with rc=${rc}"
-            exit $rc
+    assignments=`cat conf/assignment.txt`
+    for assignment in $assignments; do
+        if [ -f ./assignment-autotest/test/${assignment}/assignment-test.sh ]; then
+            echo "Executing assignment test script for $assignment"
+            ./assignment-autotest/test/${assignment}/assignment-test.sh $test_dir
+            rc=$?
+            if [ $rc -eq 0 ]; then
+                echo "Test of assignment ${assignment} complete with success"
+            else
+                echo "Test of assignment ${assignment} failed with rc=${rc}"
+                exit $rc
         fi
-    else
-        echo "No assignment-test script found for ${assignment}"
-        exit 1
-    fi
+        else
+            echo "No assignment-test script found for ${assignment}"
+            exit 1
+        fi
+    done
 else
     echo "Missing conf/assignment.txt, no assignment to run"
     exit 1

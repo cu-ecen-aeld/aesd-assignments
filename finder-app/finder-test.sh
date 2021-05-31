758fe00c474d7f1becbc9894997d7e2fec57b930
Dan Walkes <danwalkes@trellis-logic.com>
2021-05-31 15:25:03 -0600

    Use shell script for finder implementation

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -47,7 +47,7 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
